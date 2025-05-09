646e8867598449a4b21b2725f300e2754f7404b2
Malo Tamalet <malo.tamalet.2023@polytechnique.org>
2025-05-09 14:26:43 +0200

    new data

--- Étude du signal/Acquisition.py
+++ Étude du signal/Acquisition.py
@@ -135,13 +135,13 @@ if SAVE_DATA:
     channels_used = "_".join(active_channels.keys())
     
     # Création du nom de fichier avec la date et les canaux utilisés
-    filename = f"/Users/malotamalet/Desktop/2A/Modal/Modal/Temps de vie/Data_{date_str}_{channels_used}.csv"
+    filename = f"/Users/malotamalet/Desktop/2A/Modal/Modal/Temps de vie/Data/Data_{date_str}_{channels_used}.csv"
 
     df.to_csv(filename, index=False)
     print(f"Données enregistrées dans {filename}")
 
     # Enregistrement du plot dans le même dossier
-    plot_filename = f"/Users/malotamalet/Desktop/2A/Modal/Modal/Temps de vie/Plot_{date_str}_{channels_used}.png"
+    plot_filename = f"/Users/malotamalet/Desktop/2A/Modal/Modal/Temps de vie/Data/Plot_{date_str}_{channels_used}.png"
     plt.savefig(plot_filename, bbox_inches='tight')
     print(f"Plot enregistré dans {plot_filename}")
 
